5e4e458b092afaf8857b72b6233bb778917f5ada
Admin <hairais@mail.ru>
2025-03-09 01:40:29 +0700

    Minors

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,33 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import svgr from 'vite-plugin-svgr'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
   server: {
-    host: '0.0.0.0'
-  }
+    host: '0.0.0.0',
+  },
+  base: '/',
+  build: {
+    // Вывод сборки будет в папку "static"
+    // outDir: 'static',
+    // Убираем вложенную папку для ассетов, чтобы все файлы (js, css, изображения) оказались прямо в "static"
+    assetsDir: ''
+  },
+  plugins: [react(), svgr({
+    svgrOptions: {
+      // ...
+    },
+
+    // esbuild options, to transform jsx to js
+    esbuildOptions: {
+      // ...
+    },
+
+    // A minimatch pattern, or array of patterns, which specifies the files in the build the plugin should include.
+    include: "**/*.svg?react",
+
+    // A minimatch pattern, or array of patterns, which specifies the files in the build the plugin should ignore. By default no files are ignored.
+    exclude: "",
+  })]
 })
