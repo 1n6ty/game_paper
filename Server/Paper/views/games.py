5391aa08ec7d880dfbbd9a354afaf9cd3d35e3a6
1n6ty <artyom.inety@gmail.com>
2025-03-18 01:23:04 +0700

    refractoring game

--- Server/Paper/views/games.py
+++ Server/Paper/views/games.py
@@ -47,7 +47,7 @@ def get_game_links(req: HttpRequest) -> JsonResponse | HttpResponse:
 
         return JsonResponse(
             {
-                g.name: [g.paint_script.name, g.icon.name] for g in games
+                g.name: {"draw_url": g.paint_script.name, "cover_url": g.icon.name} for g in games
             }
         )
     return HttpResponse(status=400)
