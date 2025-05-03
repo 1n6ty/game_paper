b8cfe34c21e31e680bdff4a54247a5557d3757e6
Admin <hairais@mail.ru>
2025-05-03 19:44:56 +0700

    Added route config to App.jsx

--- src/ui/App.jsx
+++ src/ui/App.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
-import { Routes, Route } from "react-router-dom";
-import TabBar from "./components/TabBar/TabBar";
+import { useRoutes } from "react-router-dom";
+
+import BottomNavBar from "./components/BottomNavBar/BottomNavBar";
 
 import Home from "./pages/Home/Home";
 import Games from "./pages/Games/Games";
@@ -9,9 +10,46 @@ import Game from "./pages/Game/Game";
 
 import { UserProvider } from "./contexts/UserContext";
 
+import HomeIcon from "../assets/icons/Home.svg?react";
+import GamesIcon from "../assets/icons/Games.svg?react";
+import ScannerIcon from "../assets/icons/Qr.svg?react";
+
 import "./App.css";
 
+const routeConfig = [
+  {
+    path: "/",
+    element: <Home />,
+    label: "Главная",
+    svgr: <HomeIcon />,
+    showInTab: true,
+  },
+  {
+    path: "/games",
+    element: <Games />,
+    label: "Игры",
+    svgr: <GamesIcon />,
+    showInTab: true,
+  },
+  {
+    path: "/scanner",
+    element: <Scanner />,
+    label: "Сканер",
+    svgr: <ScannerIcon />,
+    showInTab: true,
+  },
+  {
+    path: "/games/:gameName",
+    element: <Game />,
+    // не отображаем в табах
+    showInTab: false,
+  },
+];
+
 export default function App() {
+  const element = useRoutes(
+    routeConfig.map(r => ({ path: r.path, element: r.element }))
+  );
 
   useEffect(() => {
     if (window.Telegram?.WebApp) {
@@ -22,19 +60,16 @@ export default function App() {
     }
   }, []);
 
+  const tabs = routeConfig.filter(route => route.showInTab);
+
   return (
     <UserProvider>
       <div className="app-container">
         <main className="content">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/games" element={<Games />} />
-            <Route path="/scanner" element={<Scanner />} />
-            <Route path="/games/:gameName" element={<Game />} />
-          </Routes>
+          {element}
         </main>
-        
-        <TabBar />
+
+        <BottomNavBar tabs={tabs} />
       </div>
     </UserProvider>
   );
