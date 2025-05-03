2fd7b8af952ed390c8d3d565bc68e3c385392781
Admin <hairais@mail.ru>
2025-05-03 23:23:07 +0700

    New Activities page, but empty

---
+++ src/ui/pages/Activities/Activities.jsx
@@ -0,0 +1,11 @@
+import "./Activities.css";
+
+export default function Activities() {
+  return (
+    <div className="activities-container">
+      <p className="activities-container__soon">
+        Данный раздел находится в разработке
+      </p>
+    </div>
+  );
+}
